fe6b5c33930cd07a3a0e64a5bc8c24437c3932b8
Mikkel Dissing <mikkeld2606@gmail.com>
2026-09-20 21:07:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import subprocess
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:\
+        return
+    await bot.process_commands(message)
+
+@bot.command()
+async def server_status(ctx):
+    result = subprocess.run(["sudo", "systemctl", "status", "minecraft"], capture_output=True)
+    print(result)
+
+bot.run(token)
