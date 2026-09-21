7d29fa8dde5bc413b9d8cb8438903bc7960925c2
Mikkel Dissing <mikkeld2606@gmail.com>
2026-09-21 17:24:49 +0200

    Testin bug

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ async def spiller_liste(ctx):
     with MCRcon("127.0.0.1", rconpass, port=25575) as mcr:
         response = mcr.command("list")
         print("Kommandoen blev koert")
+        await ctx.send("Test koersel")
         await ctx.send(response)
 
 
